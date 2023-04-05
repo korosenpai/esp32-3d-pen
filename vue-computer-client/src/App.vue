7c0c605c198c7a53bc0ffc9283fdfd0049ea22b7
korosenpai <koro.dev404@gmail.com>
2023-04-05 20:32:47 +0200

    basic reading serial port

--- vue-computer-client/src/App.vue
+++ vue-computer-client/src/App.vue
@@ -6,6 +6,23 @@
 <script setup>
 import ThreejsDemo from "@/components/ThreejsDemoSpin.vue"
 
+
+const { SerialPort } = require('serialport')
+const { ReadlineParser } = require("@serialport/parser-readline")
+
+// TODO check validity of https://stackoverflow.com/questions/66062682/serial-port-with-electron-vue-js
+
+const port = new SerialPort({
+    path: "com10",
+    baudRate: 115200,
+})
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }))
+
+parser.on("data", data => {
+    console.log(data)
+})
+
 </script>
 
 <style lang="scss">
--- vue-computer-client/vue.config.js
+++ vue-computer-client/vue.config.js
@@ -1,13 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true
 })
 
+// serialport -> https://stackoverflow.com/questions/66062682/serial-port-with-electron-vue-js
 module.exports = {
-  pluginOptions: {
-    electronBuilder: {
-      customFileProtocol: './',
-      nodeIntegration: true
+    pluginOptions: {
+        electronBuilder: {
+            externals: ["serialport"],
+            customFileProtocol: './',
+            nodeIntegration: true
+        }
     }
-  }
 }
