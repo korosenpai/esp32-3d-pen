8432b01e363906b8ed4af7a90cdf745bb3172179
korosenpai <koro.dev404@gmail.com>
2023-04-05 17:19:09 +0200

    working threejs demo

--- vue-computer-client/src/App.vue
+++ vue-computer-client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <!-- <img alt="Vue logo" src="@/assets/logo.png"> -->
-  <ThreejsDemo />
+    <!-- <img alt="Vue logo" src="@/assets/logo.png"> -->
+    <ThreejsDemo />
 </template>
 
 <script setup>
@@ -10,11 +10,13 @@ import ThreejsDemo from "@/components/ThreejsDemoSpin.vue"
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    // text-align: center;
+    // color: #2c3e50;
+    // margin-top: 60px;
+    margin: 0;
+    padding: 0;
 }
 </style>
--- vue-computer-client/src/components/ThreejsDemoSpin.vue
+++ vue-computer-client/src/components/ThreejsDemoSpin.vue
@@ -2,53 +2,89 @@
     <div id="container"></div>
 </template>
 
-<script>
-import * as Three from 'three'
+<script setup>
+import * as THREE from 'three'
+import { onMounted } from 'vue';
 
-export default {
-  name: 'ThreeTest',
-  data() {
-    return {
-      camera: null,
-      scene: null,
-      renderer: null,
-      mesh: null
-    }
-  },
-  methods: {
-    init: function() {
-        let container = document.getElementById('container');
+let scene
+let camera
+let renderer
+let sceneObjects = []
 
-        this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10);
-        this.camera.position.z = 1;
+onMounted(() => {
 
-        this.scene = new Three.Scene();
+function init() {
+    let container = document.getElementById( "container" )
 
-        let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
-        let material = new Three.MeshNormalMaterial();
+    scene = new THREE.Scene()
 
-        this.mesh = new Three.Mesh(geometry, material);
-        this.scene.add(this.mesh);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera.position.z = 5
 
-        this.renderer = new Three.WebGLRenderer({antialias: true});
-        this.renderer.setSize(container.clientWidth, container.clientHeight);
-        container.appendChild(this.renderer.domElement);
+    renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
 
-    },
-    animate: function() {
-        requestAnimationFrame(this.animate);
-        this.mesh.rotation.x += 0.01;
-        this.mesh.rotation.y += 0.02;
-        this.renderer.render(this.scene, this.camera);
-    }
-  },
-  mounted() {
-      this.init();
-      // this.animate();
-  }
+    container.appendChild(renderer.domElement)
+    adjustLighting()
+    addBasicCube()
+    animationLoop()
 }
+
+function adjustLighting() {
+    let pointLight = new THREE.PointLight(0xdddddd)
+    pointLight.position.set(-5, -3, 3)
+    scene.add(pointLight)
+
+    let ambientLight = new THREE.AmbientLight(0x505050)
+    scene.add(ambientLight)
+}
+
+function addBasicCube() {
+    let geometry = new THREE.BoxGeometry(1, 1, 1)
+    let material = new THREE.MeshLambertMaterial()  
+
+    let mesh = new THREE.Mesh(geometry, material)
+    // mesh.position.x = -2
+    scene.add(mesh)
+    sceneObjects.push(mesh)
+}
+
+function animationLoop() {
+    renderer.render(scene, camera)
+
+    for(let object of sceneObjects) {
+        object.rotation.x += 0.01
+        object.rotation.y += 0.03
+        // object.rotation.z += 0.03
+    }
+
+    requestAnimationFrame(animationLoop)
+}
+
+init()
+
+
+window.addEventListener( 'resize', onWindowResize, false );
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+
+})
+
 </script>
 
 <style scoped lang="scss">
-    
+#container {
+    background-color: aquamarine;
+    width: 100vw;
+    height: 100vh;
+    display: block;
+    position: fixed;
+    top: 0;
+    left: 0;
+    // z-index: -9999;
+}
 </style>
