2c1d640e02f17ff8fcbfb9c2552f49aac891e2c1
korosenpai <koro.dev404@gmail.com>
2023-04-06 18:49:36 +0200

    preparing store

--- vue-computer-client/src/store/index.js
+++ vue-computer-client/src/store/index.js
@@ -1,14 +1,26 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        pitch: null,
+        roll: null,
+        magnHeading: null,
+    },
+
+    getters: {
+        pitch(state) { return state.pitch },
+        roll(state) { return state.roll },
+        magnHeading(state) { return state.magnHeading },
+    },
+
+    mutations: {
+        setPitch(state, pitch) { state.pitch = pitch },
+        setRoll(state, roll) { state.roll = roll },
+        setPMagnHeading(state, magnHeading) { state.magnHeading = magnHeading },
+    },
+    
+    actions: {
+    },
+    modules: {
+    }
 })
