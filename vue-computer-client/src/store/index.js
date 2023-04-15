a3c8b261b14ec413b61cfa481a1ca817fa960059
korosenpai <koro.dev404@gmail.com>
2023-04-15 21:55:00 +0200

    added accel

--- vue-computer-client/src/store/index.js
+++ vue-computer-client/src/store/index.js
@@ -2,18 +2,21 @@ import { createStore } from 'vuex'
 
 export default createStore({
     state: {
-        pitch: null,
-        roll: null,
-        magnHeading: null,
+        accel: [0, 0, 0],
+        pitch: 0,
+        roll: 0,
+        magnHeading: 0,
     },
 
     getters: {
+        accel(state) { return state.accel },
         pitch(state) { return state.pitch },
         roll(state) { return state.roll },
         magnHeading(state) { return state.magnHeading },
     },
 
     mutations: {
+        setAccel(state, accel) { state.accel = accel },
         setGyro(state, gyro) {
             state.pitch = gyro[0]
             state.roll = gyro[1]
